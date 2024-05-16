2a9d5a0bc2fae7bc429b04dad3908184a37e071e
Darren Ting <darren@mlperformance.co.uk>
2024-05-16 11:17:57 +0800

    update: router

--- app/main.py
+++ app/main.py
@@ -1,106 +1,38 @@
-from fastapi import FastAPI, Response, status, HTTPException, Depends
-from fastapi.params import Body
-from pydantic import BaseModel
-
-from typing import Optional, List
-from random import randrange
+from fastapi import FastAPI
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
-from . import models, schemas, utils
-from .database import engine, get_db
-from sqlalchemy.orm import Session
 
+from app import models
+from app.database import engine
+from app.routers import post, user
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-
-
 while True:
     try:
         conn = psycopg2.connect(
-            host = "localhost", 
-            database = "fastapi", 
-            user = "postgres", 
-            password = "kyoebrn98",
-            cursor_factory = RealDictCursor
-            )
+            host="localhost",
+            database="fastapi",
+            user="postgres",
+            password="kyoebrn98",
+            cursor_factory=RealDictCursor,
+        )
         cursor = conn.cursor()
-        print('Successfully connect to database')
+        print("Successfully connect to database")
         break
     except Exception as error:
         print("Failed to connect to database")
-        print(f'Error: {error}')
+        print(f"Error: {error}")
         time.sleep(2)
 
-def find_post(id):
-    for p in my_posts:
-        if p['id'] == id:
-            return p
 
-def find_index_post(id):
-    for i, p in enumerate(my_posts):
-        if p['id'] == id:
-            return i
+app.include_router(post.router)
+app.include_router(user.router)
+
 
 @app.get("/")
 def root():
     return {"message": "Welcome to my api!"}
-
-@app.get("/posts", response_model=List[schemas.Post])
-def get_posts(db: Session = Depends(get_db)):
-    posts = db.query(models.Post).all()
-    return posts
-
-
-@app.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
-def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
-    new_post = models.Post(**post.dict())
-    db.add(new_post)
-    db.commit()
-    db.refresh(new_post)
-    return new_post
-
-@app.get("/posts/{id}", response_model=schemas.Post)
-def get_post(id: int, db: Session = Depends(get_db)):
-    post = db.query(models.Post).filter(models.Post.id == id).first()
-    if post == None:
-        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, 
-                            detail = f'Cannot find post with id {id}')
-    return post
-
-@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int, db: Session = Depends(get_db)):
-    post = db.query(models.Post).filter(models.Post.id == id)
-
-    if post.first() == None:
-        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, 
-                            detail = f'post with id {id} does not exist')
-    post.delete(synchronize_session = False)
-    db.commit()
-    return Response(status_code = status.HTTP_204_NO_CONTENT)
-
-@app.put("/posts/{id}", response_model=schemas.Post)
-def update_post(id: int, udpated_post: schemas.PostUpdate, db: Session = Depends(get_db)):
-    post_query = db.query(models.Post).filter(models.Post.id == id)
-    post = post_query.first()
-    db.commit()
-    if post == None:
-        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, 
-                            detail = f'post with id {id} does not exist')
-    post_query.update(udpated_post.dict(), synchronize_session=False)
-    db.commit()
-    return post_query.first()
-
-@app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
-def create_users(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    
-    hashed_password = utils.hash(user.password)
-    user.password = hashed_password
-    new_user = models.User(**user.dict())
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    return new_user
---
+++ app/routers/user.py
@@ -0,0 +1,32 @@
+from fastapi import status, HTTPException, Depends, APIRouter
+from sqlalchemy.orm import Session
+
+from app import models, schemas, utils
+from app.database import get_db
+
+router = APIRouter()
+
+
+@router.post(
+    "/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut
+)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+
+    hashed_password = utils.hash(user.password)
+    user.password = hashed_password
+    new_user = models.User(**user.model_dump())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+
+@router.get("/users/{id}", response_model=schemas.UserOut)
+def get_post(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if user == None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Cannot find user with id {id}",
+        )
+    return user
